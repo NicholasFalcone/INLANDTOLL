0bff658396db5a2bc12f6daf53f3e4b339164974
Nicholas <nicholasfal19@gmail.com>
2026-08-25 21:24:19 +0200

    Reworking on Intectaction

--- Source/InlandToll/BaseAnomaly.cpp
+++ Source/InlandToll/BaseAnomaly.cpp
@@ -3,3 +3,14 @@
 
 #include "BaseAnomaly.h"
 
+void ABaseAnomaly::OnInteract()
+{
+    Super::OnInteract();
+
+    CurrentInspectionTime = 0.0f; // Reset the current inspection time when interacting with the anomaly
+
+    // Log the inspectionMaxTime value for debugging
+    UE_LOG(LogTemp, Warning, TEXT("Max Inspection Time: %f"), MaxInspectionTime);
+
+    // Additional logic for anomaly interaction can be added here
+}
--- Source/InlandToll/BaseAnomaly.h
+++ Source/InlandToll/BaseAnomaly.h
@@ -3,15 +3,24 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "BaseInteractable.h"
+#include "InspectionProp.h"
 #include "BaseAnomaly.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class INLANDTOLL_API ABaseAnomaly : public ABaseInteractable
+class INLANDTOLL_API ABaseAnomaly : public AInspectionProp
 {
 	GENERATED_BODY()
 	
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Anomaly Details")
+	float MaxInspectionTime = -1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Anomaly Details")
+	float CurrentInspectionTime = 0.0f;
+
+	virtual void OnInteract() override;
+
 };
